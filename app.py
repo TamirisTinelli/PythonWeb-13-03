c95a371a8594dc8d4980fc5cc5b9a360a2622430
TamirisTinelli <tamiris.tinelli@usp.br>
2024-03-28 19:59:07 -0300

    Criação do banco de dados sql

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask("blog")
 
 @app.route("/")
 def blog():
+    # sql = "SELECT titulo, texto, data_criacao FROM posts ORDER BY id DESC"
     posts = [{"titulo": "Meu titulo", "texto": "Meu texto", "data_criacao":"27/03/2024"},
              {"titulo": "Meu titulo2", "texto": "Meu texto2", "data_criacao":"26/03/2024"}]
     return render_template("exibir_posts.html", post=posts)
