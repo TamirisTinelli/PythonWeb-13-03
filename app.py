7001bb8d096b123024a35050563e542ee942b27c
TamirisTinelli <tamiris.tinelli@usp.br>
2024-03-28 19:54:23 -0300

    Criação da página de login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g, request, redirect, session, url_for, flash, abort
 
 app = Flask("blog")
 
@@ -7,3 +7,11 @@ def blog():
     posts = [{"titulo": "Meu titulo", "texto": "Meu texto", "data_criacao":"27/03/2024"},
              {"titulo": "Meu titulo2", "texto": "Meu texto2", "data_criacao":"26/03/2024"}]
     return render_template("exibir_posts.html", post=posts)
+
+@app.route("/login",  methods=["POST", "GET"])
+def login():
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
