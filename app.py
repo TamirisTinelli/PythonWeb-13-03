715eff72d7fa92f4f80fd035e8712f69929849ea
TamirisTinelli <tamiris.tinelli@usp.br>
2024-03-28 11:04:02 -0300

    criação da estrutura de blog

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
 
-app = Flask("Olá")
+app = Flask("blog")
 
 @app.route("/")
-def ola():
-    return "Olá mundo, grande mundo"
-
-@app.route("/alunos")
-def alunos():
-    return render_template("hello.html")
+def blog():
+    posts = [{"titulo": "Meu titulo", "texto": "Meu texto", "data_criacao":"27/03/2024"},
+             {"titulo": "Meu titulo2", "texto": "Meu texto2", "data_criacao":"26/03/2024"}]
+    return render_template("exibir_posts.html", post=posts)
